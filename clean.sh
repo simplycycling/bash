ceeb6ab08bd0d2717a63ccf6c41c5fe83df76797
Roger Sherman <coolgrooves@hotmail.com>
2013-05-23 21:48:17 -0400

    Tricky backup script, with a lot of work to be done yet

---
+++ clean.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# The purpose of this script is to keep the Chromium cache from getting out
+# of hand.
+
+du -h --max-depth=1 /home/rog/.cache/chromium/Default/Cache/
+
+# Pseudocode for the next part of this script
+#
+# If the cache is greater than 100 MB
+# Then
+# 	If any cache is more than two days old
+# 	then delete and echo x amount of files were cleaned up
+# 	else if cache is less than two days old
+#	then echo this can ripen for a few more days
+#
+# If the cache is less than 100 MB
+# Then echo that there is plenty of free space
+
+sleep 7
+
+echo 'Finish writing that script, boy!'
+sleep 3
