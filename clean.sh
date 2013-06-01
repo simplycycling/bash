a9afdad4314e872a001502ff31bea9bd7bb4ed69
Roger Sherman <coolgrooves@hotmail.com>
2013-06-01 02:21:36 -0400

    Taking the pseudocode out of the comments, making comments that reflect what's in there.

--- clean.sh
+++ clean.sh
@@ -5,19 +5,7 @@
 
 x=$(du ~/.cache/chromium/Default/Cache/ | awk '{print $1}')
 
-# Pseudocode for the next part of this script
-#
-# If the cache is greater than 100 MB
-# Then
-# 	If any cache is more than two days old
-# 	then delete and echo x amount of files were cleaned up
-# 	else if cache is less than two days old
-#	then echo this can ripen for a few more days
-#
-# If the cache is less than 100 MB
-# Then echo that there is plenty of free space
-#
-# But in the meantime, we'll just do this:
+# this is to delete files that are more than two days old
 
 find ~/.cache/chromium/Default/Cache/ -mindepth 1 -type f -mtime +2 -delete
 
