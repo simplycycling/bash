8be90af39c74a0771e28b77429d695c15f4fe936
Roger Sherman <rsherman@viddler.com>
2016-08-31 22:33:59 +1000

    Cleaned up minor dodgyness.

--- gpg-bounce.sh
+++ gpg-bounce.sh
@@ -6,6 +6,6 @@ echo "$gpgproc"
 kill -9 "$gpgproc"
 if [ -f "$file" ]
 then
-    cp $file gpg-agent.bak.$(date +"%m_%d_%Y")
+    cp ${file} gpg-agent.bak.$(date +"%m_%d_%Y")
 fi
 gpg-agent --daemon --enable-ssh-support --write-env-file ~/.gpg-agent-info
