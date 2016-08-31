ad284a2b6de35d5a36a4ecc6d11c83e858635e77
Roger Sherman <rsherman@viddler.com>
2016-08-31 21:47:04 +1000

    Modified script to back up an existing .gpg-agent-info.

--- gpg-bounce.sh
+++ gpg-bounce.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 gpgproc=$(pgrep gpg-agent)
+file=".gpg-agent-info"
+cd ~/
 echo "$gpgproc"
 kill -9 "$gpgproc"
+if [ -f "$file" ]
+then
+    cp $file gpg-agent.bak
+fi
 gpg-agent --daemon --enable-ssh-support --write-env-file ~/.gpg-agent-info
