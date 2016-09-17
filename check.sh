68dfe9915a11deea53edf0f1ae9466c4e6c01521
Roger Sherman <rsherman@viddler.com>
2016-09-17 17:33:09 +1000

    Added a check to make sure the backed up file exists.

---
+++ check.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ -f ~/Google\ Drive/passBackup/passBackup-$(date +%m-%d-%Y).tgz ]
+then
+    echo "Success!"
+else
+    echo "your so speshul"
+fi
--- passBackup.sh
+++ passBackup.sh
@@ -18,7 +18,12 @@ if [ ${value} -gt 5 ]
 then
 	/usr/bin/find /Users/rsherman/Google\ Drive/passBackup -mtime +5 -exec rm {} \;
 fi
-# That's it!
+# That's it! Now, let's make sure the file exists - if not, you will receive a rather rude message.
 /bin/echo " "
-/bin/echo "All pass files backed up, sir!"
+if [ -f ~/Google\ Drive/passBackup/passBackup-$(date +%m-%d-%Y).tgz ]
+then
+    /bin/echo "All pass files backed up, sir!"
+else
+    /bin/echo "your so speshul"
+fi
 
