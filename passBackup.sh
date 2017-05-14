2ec5c99d9fd00aad9eec6841a914c17051fd9d4c
Roger Sherman <rsherman@linux.com>
2017-05-14 17:32:24 +1000

    Adjusted for the new environment

--- passBackup.sh
+++ passBackup.sh
@@ -4,23 +4,23 @@
 # Full file paths necessary, as this is used in
 # a cron job.
 
-/usr/bin/cd /Users/rsherman
+cd /home/rsherman
 
 # First, we tar everything up
-/usr/bin/tar -czf passBackup-mac-$(date +%m-%d-%Y).tgz .password-store
+/usr/bin/tar -czf passBackup-arch-$(date +%m-%d-%Y).tgz .password-store
 
 # Next we move it to Google Drive
-mv /Users/rsherman/passBackup-*.tgz /Users/rsherman/Google_Drive/passBackup/
+mv /home/rsherman/passBackup-*.tgz /home/rsherman/insync/passBackup/
 
 # Then we make sure the directory these tarballs get moved to doesn't get out of hand
-value=$( ls -l ~/Google_Drive/passBackup/ | wc -l )
+value=$( ls -l ~/insync/passBackup/ | wc -l )
 if [ ${value} -gt 5 ]
 then
-	/usr/bin/find /Users/rsherman/Google_Drive/passBackup -mtime +5 -exec rm {} \;
+	/usr/bin/find /home/rsherman/insync/passBackup -mtime +5 -exec rm {} \;
 fi
 # That's it! Now, let's make sure the file exists - if not, you will receive a rather rude message.
 /bin/echo " "
-if [ -f ~/Google_Drive/passBackup/passBackup-mac-$(date +%m-%d-%Y).tgz ]
+if [ -f ~/insync/passBackup/passBackup-arch-$(date +%m-%d-%Y).tgz ]
 then
     /bin/echo "All pass files backed up, sir!"
 else
